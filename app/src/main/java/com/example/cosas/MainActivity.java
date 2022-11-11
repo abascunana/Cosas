8fe1af3e8cae0f25bab46806ed6c811aa909864f
abascunana <91748309+abascunana@users.noreply.github.com>
2022-11-11 17:52:20 +0100

    App completada

---
+++ app/src/main/java/com/example/cosas/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.cosas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
